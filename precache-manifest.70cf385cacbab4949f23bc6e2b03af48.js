3c2e85dcf365c028d7dcf5deeb939ed70240546e
Ashwath <ashwathvenkatraman@gmail.com>
2020-12-31 02:58:56 -0500

    Updates

---
+++ precache-manifest.70cf385cacbab4949f23bc6e2b03af48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6c8eb0694dc0e0fb8b0ca4478aa9af",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13d830ef62844f42afbc",
+    "url": "/portfolio/static/css/main.9f0fa432.chunk.css"
+  },
+  {
+    "revision": "d5166e535e2d704d6a83",
+    "url": "/portfolio/static/js/2.a23cdf86.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.a23cdf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d830ef62844f42afbc",
+    "url": "/portfolio/static/js/main.17af40c6.chunk.js"
+  },
+  {
+    "revision": "711327842eda866ac99b",
+    "url": "/portfolio/static/js/runtime-main.58762675.js"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/portfolio/static/media/avatar2.daeaf731.png"
+  }
+]);
